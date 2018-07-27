312f1d64802721ff4642192d8d52084f0282301a
Dor8 <dordahan8@gmail.com>
2018-07-27 18:44:51 +0300

    add equal conditions and fix bugs

--- WireBST.java
+++ WireBST.java
@@ -122,16 +122,16 @@ public class WireBST{
      * removing a deep leaf. used by the removeWiredBSTNode function
      * @param node. the node we wish to remove
      */
-    private void removeDeepLeaf( WireBSTNode node ){
-        if ( node.getParent() == null ){
+    private void removeDeepLeaf(WireBSTNode node){
+        if (node.getParent() == null){
             _head = null;
             return;
         }
-        if ( node.equal( node.getParent().getRight())) {
-            node.getParent().setRight( node.getRight() );
+        if (node.equal(node.getParent().getRight())) {
+            node.getParent().setRight(node.getRight());
         }
         else {
-            node.getParent().setLeft( node.getLeft() );
+            node.getParent().setLeft(node.getLeft());
         }
     }
 
@@ -140,11 +140,11 @@ public class WireBST{
      * @param node. the node we wish to remove
      */
     private void removeNodeWithOnlyRightSon(WireBSTNode node){
-        if (node.getParent().getRight() == node){             // in case the node is a right son
-            node.getParent().setRight() == node.getRight();
+        if (node.getParent().getRight().equal(node)){             // in case the node is a right son
+            node.getParent().setRight(node.getRight());
         }
-        if (node.getParent().getLeft() == node){              // in case the node is a left son
-            node.getParent().setLeft() == node.getRight();
+        if (node.getParent().getLeft().equal(node)){              // in case the node is a left son
+            node.getParent().setLeft(node.getRight());
         }
     }
 
@@ -153,11 +153,11 @@ public class WireBST{
     * @param node. the node we wish to remove
     */
     private void removeNodeWithOnlyLeftSon(WireBSTNode node){
-        if (node.getParent().getRight() == node){             // in case the node is a right son
-            node.getParent().setRight() == node.getLeft();
+        if (node.getParent().getRight().equal(node)){             // in case the node is a right son
+            node.getParent().setRight(node.getLeft());
         }
-        if (node.getParent().getLeft() == node){              // in case the node is a left son
-            node.getParent().setLeft() == node.getLeft();
+        if (node.getParent().getLeft().equal(node)){              // in case the node is a left son
+            node.getParent().setLeft(node.getLeft());
         }
     }
 
@@ -172,7 +172,7 @@ public class WireBST{
         }
         if (node.getParent() == null )){
             _head = temp;
-            temp.setRight = node.getRight;
+            temp.setRight(node.getRight);
             temp.setLeft(node.getLeft);
         } else if (node.equal(node.getParent().getRight)){
             temp.setRight(node.getRight);
